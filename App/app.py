4da8b56d3d617896039cf4adda976640c3d64a0d
Alireza <armanparsi206@outlook.com>
2023-05-18 23:41:33 +0330

    Create DataBase

--- App/app.py
+++ App/app.py
@@ -2,13 +2,16 @@ from flask import Flask , render_template , redirect , url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+# Intensification Of Database Manager 
 db = SQLAlchemy()
-
+# Intensification Of Flask App
 app = Flask(__name__)
+# Set DataBase
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Database.db"
-
+# Set Flask App in DBMS
 db.init_app(app)
 
+# Database Structure 
 class Todo(db.Model):
     Id = db.Column(db.Integer, primary_key=True)
     Title = db.Column(db.String(100) , unique=True, nullable=False)
@@ -16,7 +19,11 @@ class Todo(db.Model):
     CreationTime = db.Column(db.DateTime , default=datetime.now , unique=False , nullable=False)
     ReminderTime = db.Column(db.DateTime , unique=False , nullable=False)
 
+# Create DataBase
+with app.app_context() :
+    db.create_all()
 
+# HomePage
 @app.route('/')
 def home():
     return render_template('home.html' , title='Home')
