163571dc976a387c5e7ca989303349d9410c9ba8
Alireza <armanparsi206@outlook.com>
2023-05-18 23:38:03 +0330

    Implementation Of The Database Structure

--- App/app.py
+++ App/app.py
@@ -9,6 +9,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Database.db"
 
 db.init_app(app)
 
+class Todo(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.String(100) , unique=True, nullable=False)
+    Content = db.Column(db.Text , unique=False , nullable=False)
+    CreationTime = db.Column(db.DateTime , default=datetime.now , unique=False , nullable=False)
+    ReminderTime = db.Column(db.DateTime , unique=False , nullable=False)
+
+
 @app.route('/')
 def home():
     return render_template('home.html' , title='Home')
