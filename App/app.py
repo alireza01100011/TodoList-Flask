d0c1b7a22b2b332fc4b99336b6e0ac0c7239fbc9
Alireza <armanparsi206@outlook.com>
2023-05-18 16:27:11 +0330

    Config Database

--- App/app.py
+++ App/app.py
@@ -2,8 +2,12 @@ from flask import Flask , render_template , redirect , url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Database.db"
+
+db.init_app(app)
 
 @app.route('/')
 def home():
